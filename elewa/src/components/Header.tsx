e3017431199d051a17c60f21d15ec18b23f41a9c
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-11-02 21:16:17 +0300

    :construction: Added primitive code for Header component

---
+++ elewa/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const  Header :React.FC=()=>{
+    //Header is initialized as type React Function Component
+    return (
+        <div>
+            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg" alt=""/>
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Social Impact</p>
+            <p>Invest</p>
+            <p>Venture Labs</p>
+            <p>Brands</p>
+            <p>Careers</p>
+            <p>Contacts</p>
+
+        </div>
+    )
+}
+
+export default Header
