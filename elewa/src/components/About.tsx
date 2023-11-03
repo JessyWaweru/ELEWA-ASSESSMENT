c1366637a9e8df563e0014c96635e7689cb82109
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-11-03 07:09:08 +0300

    :construction: Added background image to  the About component

--- elewa/src/components/About.tsx
+++ elewa/src/components/About.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import Header from './Header'
 const About :React.FC=()=>{
 return(
-    <>
-    <Header/>
-     <img  alt="t" className="backgroundImage" src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1679650530/elewa-team_digqu3.jpg"/>
+    < >
+      <div className="bg-cover bg-center h-screen "
+      style={{
+        backgroundImage:`url('https://res.cloudinary.com/dyl3rncv3/image/upload/v1679650530/elewa-team_digqu3.jpg')`
+      }}>
+        {/* above styles fpr the background image*/}
+        <div className='absolute inset-0 bg-black opacity-30'><Header/></div>
+         {/* above styles to have the background image appear darker*/}
+      </div>
+  
     </>
 )
 }
