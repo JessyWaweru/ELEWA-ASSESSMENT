af2e68a128f39ff1504a38514a5e3a66e700f750
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-11-03 09:22:31 +0300

    :construction: Added more features with related images   to  the About component

--- elewa/src/components/About.tsx
+++ elewa/src/components/About.tsx
@@ -25,14 +25,29 @@ return(
 
         
       </div>
-      <div className='mr-12 ml-12 pt-2 opacity-100 text-black '>
+      <div className='mr-12 ml-12 pt-2 opacity-100 text-black mb-20 '>
         <p className='text-[50px] pb-5'>We Care!</p>
         <div className='flex space-x-32 text-[24px]'>{/* to align items horizontally*/}
             <p>Elewa is a mission-driven organization. We <br/> make use of our cooperative and shared <br/>culture to drive the needle for the <br/>development of people and environment. We <br/>care about our growth, but also care deeply <br/>about the context surrounding us.</p>
             <p>Our investments are therefore not limited to<br/>  internal ones, but contribute heavily to our <br/> community and environment. From training<br/>  the next scout leaders on sustainable<br/>  practices, to bridging the employment gap for <br/> junior software developers.</p>
         </div>
       </div>
-  
+      <div className='flex space-x-40 w-1/2 h-1/2 mr-12 ml-12 mb-7'>{/* added images horizontally */}
+        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1680180166/elewa-group-website/Icons/PNG/Holistic_118_ylipr0.png'  alt=''/>
+        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png' alt=''/>
+        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690299/elewa-group-website/Icons/PNG/Opendata_fe7h3j.png' alt=''/>
+
+      </div>
+      <div className='mr-12 ml-12 flex space-x-52 text-black text-[32px]'>
+        <p>Holistic Solutions</p>
+        <p>Impact</p>
+        <p>Open Data</p>
+      </div>
+      <div className='mr-12 ml-12 flex space-x-8 text-black text-[21px]'>
+        <p>We go beyond a simple patch-up but<br/> develop lasting solutions through <br/>holistic design.</p>
+        <p>Impact as a direct, or indirect, result.<br/> All our respective organizations have <br/>underlying theories of change.</p>
+        <p>Sharing is caring. We share what we<br/> learn. As proof, we've open-sourced all<br/> our internal projects.</p>
+      </div>
     </>
 )
 }
