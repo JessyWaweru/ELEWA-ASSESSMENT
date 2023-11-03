bae1e0d6e78068172b1291e1ab8fcc515a1c0d2d
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-11-03 06:22:35 +0300

    :sparkles: created the Header components with related styles

--- elewa/src/components/Header.tsx
+++ elewa/src/components/Header.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 const  Header :React.FC=()=>{
     //Header is initialized as type React Function Component
+    //Tailwind css is being used
     return (
-        <div >
-
-            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg" className=' flex justify-start mt-12 pl-16 filter grayscale brightness-0'alt=""/>
-            
-            <div className='flex justify-end -mt-8 text-[12px]'> 
-                <p className='text-red-500 pl-15'>Home</p>
-            <p className='text-blue-500 ml-10'>About Us</p>
+        <div className='mr-12 ml-12 py-8 border-b' >
+{/* above style to render everything inside the div with margin right of 12 with border */}
+            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg" className=' flex justify-start mt-10 'alt=""/>
+            {/* styles above to get image to be to the far left of the web page with a margin top of 12 and padding left of 16*/}
+            <div className='flex justify-end space-x-8 -mt-8 text-[12px] text-white'> 
+            {/* styles above to get div elements to be to the right of the web page with equal spacing of 2rem,margin top 8 with specified font size*/}
+                <p className=''>Home</p>
+            <p className=''>About Us</p>
             <p>Social Impact</p>
             <p>Invest</p>
             <p>Venture Labs</p>
             <p>Brands</p>
             <p>Careers</p>
-            <p className='-pr-100'>Contacts </p>
+            <p className=''>Contacts </p>
             </div>
             
 
