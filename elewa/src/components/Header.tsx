431d71e4247ae91aff548b8fdda19f6bd89b5904
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-11-03 05:49:47 +0300

    :construction: :heavy_plus_sign: Added tailwindcss to my project and used it to add base styles to Header component

--- elewa/src/components/Header.tsx
+++ elewa/src/components/Header.tsx
@@ -3,18 +3,23 @@ import React from 'react';
 const  Header :React.FC=()=>{
     //Header is initialized as type React Function Component
     return (
-        <div>
-            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg" alt=""/>
-            <p>Home</p>
-            <p>About Us</p>
+        <div >
+
+            <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg" className=' flex justify-start mt-12 pl-16 filter grayscale brightness-0'alt=""/>
+            
+            <div className='flex justify-end -mt-8 text-[12px]'> 
+                <p className='text-red-500 pl-15'>Home</p>
+            <p className='text-blue-500 ml-10'>About Us</p>
             <p>Social Impact</p>
             <p>Invest</p>
             <p>Venture Labs</p>
             <p>Brands</p>
             <p>Careers</p>
-            <p>Contacts</p>
+            <p className='-pr-100'>Contacts </p>
+            </div>
+            
 
-        </div>
+       </div>
     )
 }
 
