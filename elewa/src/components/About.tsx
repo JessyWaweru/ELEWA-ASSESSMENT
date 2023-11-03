e0fddc37d59282eb775d28cad2570a8cdf9f11eb
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-11-03 06:34:41 +0300

    :construction: Started off with the About component

---
+++ elewa/src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+const About :React.FC=()=>{
+return(
+    <>
+    <Header/>
+     <img  alt="t" className="backgroundImage" src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1679650530/elewa-team_digqu3.jpg"/>
+    </>
+)
+}
+
+export default About
